69fa58308f8725a544ed28e5946e44f7d7960477
Faithkebenei <faithkebs16@gmail.com>
2024-03-16 16:26:15 +0300

    Africana UI

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,13 +4,43 @@ import Header from "../../components/Header/Header";
 import CTA from "../../components/CTA/CTA";
 import Footer from "../../components/Footer/Footer";
 
+import Button from "../../components/Button/Button";
+
 import heroImg from "../../utils/images/img16.jpg";
+import image from "../../utils/images/img12.png";
 
 const Contact = () => {
+  const sendMessage = () => {};
   return (
     <div className={styles.Contact}>
       <Header image={heroImg} title="Get in Touch" text="" />
-      <div></div>
+      <div className={styles.contactContent}>
+        <h2>Contact Us</h2>
+        <div className={styles.content}>
+          <div className={styles.inputContent}>
+            <div className={styles.input}>
+              <label for="name">Name:</label>
+              <input type="text" id="name" />
+            </div>
+            <div className={styles.input}>
+              <label for="email">Email:</label>
+              <input type="text" id="email" />
+            </div>
+            <div className={styles.input}>
+              <label for="message">Message:</label>
+              <textarea name="" id="message" cols="30" rows="10"></textarea>
+            </div>
+            <div className={styles.button}>
+              <Button bgColor="#ff7c36" text="Submit" clicked={sendMessage} />
+            </div>
+          </div>
+          <div className={styles.imageContent}>
+            <div className={styles.image}>
+              <img src={image} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
       <CTA />
       <Footer link="contact" />
     </div>
